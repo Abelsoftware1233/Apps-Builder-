063556f43f9b8c87f8e15caa95191c7ff889f21d
A.A <abelsoftware123@hotmail.com>
2026-08-08 10:37:25 +0200

    Change default port from 5000 to 5050

--- app.py
+++ app.py
@@ -200,6 +200,6 @@ def github_import():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
     app.run(host="0.0.0.0", port=port, debug=debug)
